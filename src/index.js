2ed8de9001cd7cb133df2b210500b3a4e3345f8e
Andrew <uytov1991@gmail.com>
2020-09-25 15:45:15 +0300

    закончил стилизовать мобильную версию

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world!')
+const headerSnapMenuOpen = document.querySelector('.header__snap--open');
+const headerSnapMenuClose = document.querySelector('.header__snap--close');
+
+headerSnapMenuOpen.addEventListener('click', () => {
+  headerSnapMenuOpen.classList.remove('hidden-menu')
+})
+
+headerSnapMenuClose.addEventListener('click', () => {
+  headerSnapMenuOpen.classList.add('hidden-menu')
+})
